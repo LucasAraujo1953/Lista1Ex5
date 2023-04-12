c84c0f3db12d96b0d5f4de7caad8e9b5e64fd545
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-12 20:51:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            double d;
+            double media;
+
+            Console.Write("Informe o primeiro número: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Informe o segundo número: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Informe o terceiro número: ");
+            c = double.Parse(Console.ReadLine());
+            Console.Write("Informe o quarto número: ");
+            d = double.Parse(Console.ReadLine());
+
+            // OPERAÇÃO
+
+            media = (a + b + c +d ) / 4;
+
+            Console.WriteLine("A média aritmética dos quatro valores é {0}",media);
+        }
+    }
+}
